e21fdc7dc33d730c3e49ca02529fe706f1ce0ccc
MaulanaMalikIbrahim <abhiemsmile@gmail.com>
2022-09-22 13:02:15 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:ui_flutter_minimalism_login/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+  }
+}
